a829efe0b0fa28b11132f8553a93ea6f122de957
fremail <fremail@ya.ru>
2017-04-08 14:15:05 +0300

    video and pcheech API in one scene

---
+++ Assets/VideoController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var videoCont = GetComponent<VideoPlayer> ();
+		videoCont.loopPointReached += VideoCont_loopPointReached;;
+
+	}
+
+	void VideoCont_loopPointReached (VideoPlayer source)
+	{
+		Debug.Log("video ended 1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void EndReached(UnityEngine.Video.VideoPlayer vp)
+	{
+		//vp.playbackSpeed = vp.playbackSpeed / 10.0F;
+		Debug.Log("video ended");
+	}
+}
